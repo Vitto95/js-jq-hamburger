d881e0dc9e8eef13bf05f1198f77710239bab647
Vitto95 <grimaldi.vitto@gmail.com>
2021-04-19 16:54:09 +0200

    Esercizio corretto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,12 @@
 $(document).ready(function () {
   //Qui inseriamo tutto il codice JS
 
-  //Creazione flag
-  var isMenuVisible = false; //default
-
   //Creazione evento per gestire comparsa e scomparsa del menu
 
-  /*   $(".fa-bars").click(function () {
-    if (isMenuVisible) {
-      $("hamburger-menu ul").hide();
-      isMenuVisible = false;
-    } else {
-      $(".hamburger-menu ul").show();
-      isMenuVisible = true;
-    }
-  }); */
-
-  if (isMenuVisible) {
-    $(".fa-times").click(function () {
-      $("hamburger-menu ul").hide();
-    });
-    isMenuVisible = false;
-  } else {
-    $(".fa-bars").click(function () {
-      $(".hamburger-menu ul").show();
-    });
-    isMenuVisible = true;
-  }
+  $(".fa-times").click(function () {
+    $(".hamburger-menu").hide();
+  });
+  $(".fa-bars").click(function () {
+    $(".hamburger-menu").show();
+  });
 });
